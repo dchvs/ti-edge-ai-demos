1aaa08cab70cd293a4d23d43363fa9b1988376fe
Daniel Chaves <daniel.chaves@ridgerun.com>
2021-06-22 15:26:20 -0400

    example: Add the example file

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+#  Copyright (C) 2021 RidgeRun, LLC (http://www.ridgerun.com)
+#  All Rights Reserved.
+#  Authors: Daniel Chaves <daniel.chaves@ridgerun.com>
+#           Marisol Zeledon <marisol.zeledon@ridgerun.com>
+#
+#  The contents of this software are proprietary and confidential to RidgeRun,
+#  LLC.  No part of this program may be photocopied, reproduced or translated
+#  into another programming language without prior written consent of
+#  RidgeRun, LLC.  The user is free to modify the source code after obtaining
+#  a software license from RidgeRun.  All source code changes must be provided
+#  back to RidgeRun without any encumbrance.
+
+from imagehandler import *
+
+
+def main():
+    image_handler = ImageHandler()
+    img = image_handler.loadImage("linux.jpg")
+    image_handler.saveImage("linux_copy.jpg", img)
+
+
+if __name__ == "__main__":
+    main()
