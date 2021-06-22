1f149dbc653f59f9175e67bc21c94d260e82e5bf
Marisol Zeledon <marisol.zeledon@ridgerun.com>
2021-06-22 09:53:56 -0600

    preprocess-parent: Add class basic structure

---
+++ preprocess.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+#  Copyright (C) 2021 RidgeRun, LLC (http://www.ridgerun.com)
+#  All Rights Reserved.
+#  Authors: Daniel Chaves <daniel.chaves@ridgerun.com>
+#           Marisol Zeledon <marisol.zeledon@ridgerun.com>
+#
+#  The contents of this software are proprietary and confidential to RidgeRun,
+#  LLC.  No part of this program may be photocopied, reproduced or translated
+#  into another programming language without prior written consent of
+#  RidgeRun, LLC.  The user is free to modify the source code after obtaining
+#  a software license from RidgeRun.  All source code changes must be provided
+#  back to RidgeRun without any encumbrance.
+
+import yaml
+import cv2
+import numpy as np
+
+
+class PreProcess:
+    def __init__(self, img, model_dir):
+        with open(model_dir + 'param.yaml', 'r') as f:
+            params = yaml.safe_load(f)
+
+        self.img = img
+
+        # preprocess params
+        self.resize = params['preprocess']['resize']
+        crop = params['preprocess']['crop']
+        if (type(crop) == int):
+            self.crop = (crop, crop)
+        self.mean = params['preprocess']['mean']
+        self.scale = params['preprocess']['scale']
+        self.reverse_channels = params['preprocess']['reverse_channels']
+        self.data_layout = params['preprocess']['data_layout']
+
+    def resize(self, img, new_width, new_height):
+
+    def channel_swap_bgr_to_rgb(self, img):
+
+    def change_format(self, img, fin, fout):
+
+    def subtract_mean_and_scale(self, img, mean, scale, chan_axis):
