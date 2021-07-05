1cb3a5c056270a21949b6610e4fca9c204c8d870
Daniel Chaves <daniel.chaves@ridgerun.com>
2021-07-05 09:06:27 -0600

    media_manager: Add the CreatePipeline

---
+++ gstreamer/media_manager.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2.7
+
+#  Copyright (C) 2021 RidgeRun, LLC (http://www.ridgerun.com)
+#  Authors: Daniel Chaves <daniel.chaves@ridgerun.com>
+#           Marisol Zeledon <marisol.zeledon@ridgerun.com>
+
+from gi.repository import Gst as gst
+from gi.repository import GLib
+import gi
+gi.require_version('Gst', '1.0')
+gi.require_version('GLib', '2.0')
+
+
+class GstManager():
+    """
+    Constructor for the Media Gstreamer Manager object
+    Class that creates the Gstreamer handler
+    """
+
+    def __init__(self):
+        # Init the gst
+        gst.init(None)
+
+        self.pipeline = None
+
+    def CreatePipeline(self, desc):
+        self.pipeline = gst.parse_launch(desc)
+
+        if self.pipeline is None:
+            return False
+
+        return True
---
+++ gstreamer/tests/test_media_manager.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+#  Copyright (C) 2021 RidgeRun, LLC (http://www.ridgerun.com)
+#  Authors: Daniel Chaves <daniel.chaves@ridgerun.com>
+#           Marisol Zeledon <marisol.zeledon@ridgerun.com>
+
+from gstreamer.media_manager import *
+import unittest
+
+
+class TestGstManager(unittest.TestCase):
+    def testCreatePipeline(self):
+        desc = "videotestsrc ! fakesink"
+
+        gst_manager = GstManager()
+
+        ret = gst_manager.CreatePipeline(desc)
+        assert(ret is not None)
+        assert(gst_manager.pipeline is not None)
+
+
+if __name__ == '__main__':
+    unittest.main()
