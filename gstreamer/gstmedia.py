2cb1cfc6aae9bffc6fe138a94792e827accda54b
Daniel Chaves <daniel.chaves@ridgerun.com>
2021-07-13 08:54:45 -0600

    doc: Add tge GStMedia class documentation

--- gstreamer/gstmedia.py
+++ gstreamer/gstmedia.py
@@ -13,16 +13,54 @@ gi.require_version('GLib', '2.0')
 
 class GstMedia():
     """
-    Constructor for the Media Gstreamer Manager object
     Class that creates the GStreamer handler
     """
 
     def __init__(self):
+        """
+        Constructor for the Media Gstreamer Manager object
+
+        Attributes
+        ----------
+        _pipeline : GstElement
+            A private GStreamer pipeline object
+
+        Methods
+        -------
+        CreateMedia(desc : str)
+            Creates the media object from a string description
+
+        DeleteMedia()
+            Deletes the media object
+
+        PlayMedia()
+            Set the media state to playing
+
+        StopMedia()
+            Set the media state to stop
+
+        GetMedia()
+            Getter for the private media object
+        """
+
         gst.init(None)
 
         self._pipeline = None
 
     def CreateMedia(self, desc):
+        """Creates the media object from a string description
+
+        Parameters
+        ----------
+        desc : str
+            The media description to create
+
+        Raises
+        ------
+        GLib.GError, ValueError
+            If the description fails to create the media
+        """
+
         try:
             self._pipeline = gst.parse_launch(desc)
         except ValueError as e:
@@ -32,6 +70,9 @@ class GstMedia():
         return True
 
     def DeleteMedia(self):
+        """Deletes the media object
+        """
+
         if self._pipeline is not None:
             del self._pipeline
             self._pipeline = None
@@ -39,6 +80,14 @@ class GstMedia():
         return True
 
     def PlayMedia(self):
+        """Set the media state to playing
+
+        Raises
+        ------
+        GLib.GError, ValueError
+            If couldn't set the media state to playing
+        """
+
         try:
             self._pipeline.set_state(gst.State.PLAYING)
         except ValueError as e:
@@ -48,6 +97,14 @@ class GstMedia():
         return True
 
     def StopMedia(self):
+        """Set the media state to stop
+
+        Raises
+        ------
+        GLib.GError, ValueError
+            If couldn't set the media state to stop
+        """
+
         try:
             self._pipeline.set_state(gst.State.NULL)
         except ValueError as e:
@@ -57,4 +114,6 @@ class GstMedia():
         return True
 
     def GetMedia(self):
+        """Getter for the private media object
+        """
         return self._pipeline
