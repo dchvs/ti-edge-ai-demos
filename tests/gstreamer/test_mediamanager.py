57d45130e1859f7685b0403a92e90acd53d2cba5
Daniel Chaves <daniel.chaves@ridgerun.com>
2021-07-18 10:55:55 -0600

    mediamanager: Add the non success test escenario

--- tests/gstreamer/test_mediamanager.py
+++ tests/gstreamer/test_mediamanager.py
@@ -10,6 +10,7 @@ gi.require_version('GLib', '2.0')  # nopep8
 from gi.repository import Gst as gst  # nopep8
 from gi.repository import GLib  # nopep8
 
+import random
 from rr.gstreamer.mediamanager import MediaManager
 from rr.gstreamer.mediamanager import MediaManagerError
 import unittest
@@ -32,6 +33,26 @@ class TestMediaManager(unittest.TestCase):
         self.assertTrue(self.key in dict)
 
         self.assertEqual(self.media, dict[self.key])
+
+
+class TestMediaManagerFail(unittest.TestCase):
+    def setUp(self):
+        self.mediamanager = MediaManager()
+
+    def testadd_media(self):
+        with self.assertRaisesRegex(MediaManagerError, "Invalid key or media"):
+            self.mediamanager.add_media(None, None)
+
+        # Check dictionary it's empty
+        dict = self.mediamanager._get_media_dict()
+        self.assertEqual(0, len(dict))
+
+    def testremove_media(self):
+        with self.assertRaisesRegex(MediaManagerError, "Invalid key"):
+            self.mediamanager.remove_media(None)
+
+        with self.assertRaisesRegex(MediaManagerError, "Unable to find the key in the dictionary"):
+            self.mediamanager.remove_media(random.random())
 
 
 if __name__ == '__main__':
