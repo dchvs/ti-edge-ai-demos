555e66ca14efeda87a9267f888bbc4107789203d
Marisol Zeledon <marisol.zeledon@ridgerun.com>
2021-07-20 17:50:07 -0600

    logevent: Add log function

--- rr/actions/log_event.py
+++ rr/actions/log_event.py
@@ -7,8 +7,30 @@ import csv
 
 
 def log(path, media, image, inf_results, fieldnames):
-    with open(path, 'a', newline='') as f:
-        pass
+    media_name = media.get_name()
+    image_time = image.get_timestamp()
+
+    f = open(path, 'a', newline='')
+    csv_writer = csv.DictWriter(f, fieldnames=fieldnames)
+    for instance in inf_results['instances']:
+        label_max = find_max_probability(instance['labels'])
+        csv_writer.writerow({'name': media_name,
+                             'time': image_time,
+                             'label': label_max['label'],
+                             'probability': label_max['probability'],
+                             'bbox-x': instance['bbox']['x'],
+                             'bbox-y': instance['bbox']['y'],
+                             'bbox-width': instance['bbox']['width'],
+                             'bbox-height': instance['bbox']['height']})
+
+
+def find_max_probability(labels):
+    first_label = labels[0]
+    label_max_prob = first_label
+    for label in labels:
+        if label['probability'] > label_max_prob['probability']:
+            label_max_prob = label
+    return label_max_prob
 
 
 def validate_csv(path):
@@ -44,5 +66,5 @@ class LogEvent:
         self._path = validate_csv(path)
         set_file_headers(self._path)
 
-    def execute():
+    def execute(media, image, inf_results):
         pass
--- tests/actions/test_log_event.py
+++ tests/actions/test_log_event.py
@@ -6,7 +6,7 @@
 import unittest
 
 import csv
-from rr.actions.log_event import LogEvent, LogEventError, log, validate_csv, set_file_headers
+from rr.actions.log_event import LogEvent, LogEventError, log, validate_csv, set_file_headers, find_max_probability, log
 
 
 class mockMedia():
@@ -28,8 +28,7 @@ class mockImage():
 class TestLogEventSuccess(unittest.TestCase):
 
     def setUp(self):
-        media = mockMedia('test_media_name')
-        image = mockImage('2021-07-27 12:00:20')
+        open('tests/actions/test_log.csv', 'w').close()
 
     def test_validate_csv_success(self):
         csv_file_good = 'test_path/test.csv'
@@ -48,7 +47,7 @@ class TestLogEventSuccess(unittest.TestCase):
                 e.exception))
 
     def test_set_header_success(self):
-        csv_file_good = 'tests/actions/test_headers.csv'
+        csv_file_good = 'tests/actions/test_log.csv'
         fieldnames = [
             'name',
             'time',
@@ -67,6 +66,68 @@ class TestLogEventSuccess(unittest.TestCase):
 
         self.assertEqual(fieldnames, first_line)
         self.assertEqual(fieldnames, result_fieldname)
+
+    def test_max_probability_success(self):
+        labels = [{"label": 'ZZZZ', "probability": 0.99},
+                  {"label": 'AAAA', "probability": 0.01}]
+
+        label_max = find_max_probability(labels)
+        expected_label = {"label": 'ZZZZ', "probability": 0.99}
+
+        self.assertEqual(expected_label, label_max)
+
+    def test_log_success(self):
+        csv_file_good = 'tests/actions/test_log.csv'
+        media = mockMedia('test_media_name')
+        image = mockImage('2021-07-27 12:00:20')
+        inf_results = {
+            "instances": [
+                {
+                    "labels": [
+                        {
+                            "label": 'ZZZZ', "probability": 0.99}, {
+                            "label": 'AAAA', "probability": 0.01}, ], "bbox": {
+                        "x": 'X', "y": 'Y', "width": 'width', "height": 'height'}}, {
+                    "labels": [
+                        {
+                            "label": 'XXXX', "probability": 0.97}, {
+                            "label": 'WWWW', "probability": 0.01}, ], "bbox": {
+                        "x": 'X', "y": 'Y', "width": 'width', "height": 'height'}}]}
+        fieldnames = [
+            'name',
+            'time',
+            'label',
+            'probability',
+            'bbox-x',
+            'bbox-y',
+            'bbox-width',
+            'bbox-height']
+
+        log(csv_file_good, media, image, inf_results, fieldnames)
+
+        first_row_ret = [
+            'test_media_name',
+            '2021-07-27 12:00:20',
+            'ZZZZ',
+            '0.99',
+            'X',
+            'Y',
+            'width',
+            'height']
+        second_row_ret = [
+            'test_media_name',
+            '2021-07-27 12:00:20',
+            'XXXX',
+            '0.97',
+            'X',
+            'Y',
+            'width',
+            'height']
+
+        with open(csv_file_good) as f:
+            reader = csv.reader(f)
+            self.assertEqual(first_row_ret, next(reader))
+            self.assertEqual(second_row_ret, next(reader))
 
 
 if __name__ == '__main__':
