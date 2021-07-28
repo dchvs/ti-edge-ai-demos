908e91dfb6011575e5741d6bf063f0167ca51ccb
Daniel Chaves <daniel.chaves@ridgerun.com>
2021-07-27 22:30:47 -0600

    ai_manager: Add the success TestAIManagerOnNewImage tests

--- tests/ai/test_ai_manager.py
+++ tests/ai/test_ai_manager.py
@@ -42,6 +42,12 @@ def create_img(width, height, rgb_color=(0, 0, 0)):
     return img
 
 
+def send_img():
+    global width, height, color
+
+    return create_img(width, height, rgb_color=color)
+
+
 class TestAIManager(unittest.TestCase):
     def setUp(self):
         global model, width, height, disp_width, disp_height
@@ -90,6 +96,23 @@ class TestAIManagerFail(unittest.TestCase):
 
     def testpostprocess(self):
         pass
+
+
+class TestAIManagerOnNewImage(unittest.TestCase):
+    def setUp(self):
+        global model, width, height, disp_width, disp_height
+
+        self.model = model
+        self.disp_width = disp_width
+        self.disp_height = disp_height
+
+        self.ai_manager = AIManagerOnNewImage(
+            send_img, model, disp_width, disp_height)
+
+    def teston_new_media(self):
+        result_img = self.ai_manager.on_new_media(
+            send_img, self.model, disp_width, disp_height)
+        self.assertTrue(0 != result_img.size)
 
 
 if __name__ == '__main__':
