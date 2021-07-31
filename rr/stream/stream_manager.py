c042acdc0b46c5abd9e0a90b51ff4580bd3cddb6
Daniel Chaves <daniel.chaves@ridgerun.com>
2021-07-30 20:35:39 -0600

    stream_manager: Add the StreamManager skeleton

---
+++ rr/stream/stream_manager.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+#  Copyright (C) 2021 RidgeRun, LLC (http://www.ridgerun.com)
+#  Authors: Daniel Chaves <daniel.chaves@ridgerun.com>
+#           Marisol Zeledon <marisol.zeledon@ridgerun.com>
+
+
+class StreamManagerError(RuntimeError):
+    pass
+
+
+class StreamManager():
+    """
+    Class that orchestrate the stream interoperations
+
+    Attributes
+    ----------
+
+    Methods
+    -------
+    """
+
+    def __init__(self):
+        """
+        Constructor for the Stream Manager object
+        """
+
+    def (self, image):
+        """Preprocess the image
+
+        Parameters
+        ----------
+
+        Raises
+        ------
+        StreamManagerError
+        """
