b72347c442eb595548303767ceab69ad6519c1f0
Daniel Chaves <daniel.chaves@ridgerun.com>
2021-07-30 20:36:00 -0600

    stream_manager: Add the StreamManager test skeleton

---
+++ tests/stream/test_stream_manager.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+#  Copyright (C) 2021 RidgeRun, LLC (http://www.ridgerun.com)
+#  Authors: Daniel Chaves <daniel.chaves@ridgerun.com>
+#           Marisol Zeledon <marisol.zeledon@ridgerun.com>
+
+import unittest
+from unittest.mock import MagicMock
+
+
+class TestStreamManager(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def teststream(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
