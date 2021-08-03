51d690b24ca64b980677d0d51928fcb08c0294d7
Daniel Chaves <daniel.chaves@ridgerun.com>
2021-08-03 14:38:20 -0600

    stream_manager: Add the StreamMedia constructor with MediaManager/AIManager callbacks orchestration

--- rr/stream/stream_manager.py
+++ rr/stream/stream_manager.py
@@ -4,6 +4,21 @@
 #  Authors: Daniel Chaves <daniel.chaves@ridgerun.com>
 #           Marisol Zeledon <marisol.zeledon@ridgerun.com>
 
+from rr.ai.ai_manager import AIManagerOnNewImage
+from rr.gstreamer.media_manager import MediaManager
+
+model = "/opt/edge_ai_apps/models/detection/TFL-OD-200-ssd-mobV1-coco-mlperf-300x300/"
+disp_width = 2040
+disp_height = 1920
+
+
+class OnNewImage():
+    def __init__(self, ai_manager):
+        self.ai_manager = ai_manager
+
+    def __call__(self, image):
+        self.ai_manager.process_image(image, model, disp_width, disp_height)
+
 
 class StreamManagerError(RuntimeError):
     pass
@@ -20,18 +35,14 @@ class StreamManager():
     -------
     """
 
-    def __init__(self):
+    def __init__(self, ai_manager, media_manager):
         """
         Constructor for the Stream Manager object
         """
 
-    def (self, image):
-        """Preprocess the image
+        self.ai_manager = ai_manager
+        self.media_manager = media_manager
 
-        Parameters
-        ----------
+        on_new_image_cb = OnNewImage(self.ai_manager)
 
-        Raises
-        ------
-        StreamManagerError
-        """
+        self.media_manager.push_buffer(on_new_image_cb)
