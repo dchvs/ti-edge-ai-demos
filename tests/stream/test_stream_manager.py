98ae8220c9a0659352d54ed876d532a18836f5a5
Daniel Chaves <daniel.chaves@ridgerun.com>
2021-08-03 14:38:50 -0600

    stream_manager: Add the StreamMedia test to MediaManager/AIManager callbacks orchestration

--- tests/stream/test_stream_manager.py
+++ tests/stream/test_stream_manager.py
@@ -7,10 +7,40 @@
 import unittest
 from unittest.mock import MagicMock
 
+from rr.ai.ai_manager import AIManagerOnNewImage
+from rr.gstreamer.imedia import IMedia
+from rr.gstreamer.media_manager import MediaManager
+from rr.stream.stream_manager import StreamManager
+from rr.stream.stream_manager import OnNewImage
+
+model = "/opt/edge_ai_apps/models/detection/TFL-OD-200-ssd-mobV1-coco-mlperf-300x300/"
+disp_width = 2040
+disp_height = 1920
+
 
 class TestStreamManager(unittest.TestCase):
     def setUp(self):
-        pass
+        self.mock_on_new_prediction_cb = MagicMock()
+        self.mock_on_new_postprocess_cb = MagicMock()
+
+        ai_manager = AIManagerOnNewImage(model, disp_width, disp_height,
+                                         self.mock_on_new_prediction_cb,
+                                         self.mock_on_new_postprocess_cb)
+
+        desc = "videotestsrc is-live=true ! fakesink async=false"
+        key = "media1"
+        media = IMedia()
+        media.create_media(desc)
+
+        mock_on_new_image_cb = MagicMock()
+
+        media_manager = MediaManager()
+        media_manager.add_media(key, media)
+        media_manager.push_buffer(mock_on_new_image_cb)
+
+        stream_manager = StreamManager(ai_manager, media_manager)
+
+        mock_on_new_image_cb.assert_called_once()
 
     def teststream(self):
         pass
