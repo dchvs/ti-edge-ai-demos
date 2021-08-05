3ed8474dbb203be09e31e944a6b06ef3e4c67043
Daniel Chaves <daniel.chaves@ridgerun.com>
2021-08-05 12:12:04 -0600

    stream_manager: Mock the install callback real __call__ from playing MediaManager from StreamManager

--- tests/stream/test_stream_manager.py
+++ tests/stream/test_stream_manager.py
@@ -15,14 +15,23 @@ from rr.gstreamer.media_manager import MediaManager
 from rr.stream.stream_manager import StreamManager
 from rr.stream.stream_manager import OnNewImage
 
+
+class MockImage:
+    pass
+
+
 model = "/opt/edge_ai_apps/models/detection/TFL-OD-200-ssd-mobV1-coco-mlperf-300x300/"
 disp_width = 2040
 disp_height = 1920
+mock_image = MockImage()
 
 
 class MockMediaManager(MediaManager):
     def install_callback(self, callback):
         self.callback = callback
+
+    def play_media(self):
+        self.callback(mock_image)
 
 
 class TestStreamManager(unittest.TestCase):
@@ -34,6 +43,9 @@ class TestStreamManager(unittest.TestCase):
                                          self.mock_on_new_prediction_cb,
                                          self.mock_on_new_postprocess_cb)
 
+        ai_manager.process_image = MagicMock(
+            mock_image, model, disp_width, disp_height)
+
         desc = "videotestsrc is-live=true ! fakesink async=false"
         key = "media1"
 
@@ -44,6 +56,10 @@ class TestStreamManager(unittest.TestCase):
         media_manager.add_media(key, media)
 
         stream_manager = StreamManager(ai_manager, media_manager)
+
+        stream_manager.play()
+        ai_manager.process_image.assert_called_once_with(
+            mock_image, model, disp_width, disp_height)
 
 
 if __name__ == '__main__':
