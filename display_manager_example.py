ae47a64f4c7b2c67401e2f638d75e1dad894e125
Carlos Rodriguez <carlos.rodriguez@ridgerun.com>
2021-08-06 18:10:48 -0600

    Add Display Manager

---
+++ display_manager_example.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+#  Copyright (C) 2021 RidgeRun, LLC (http://www.ridgerun.com)
+#  Authors: Daniel Chaves <daniel.chaves@ridgerun.com>
+#           Marisol Zeledon <marisol.zeledon@ridgerun.com>
+
+import time
+
+from rr.display.display_manager import DisplayManager
+from rr.gstreamer.imedia import IMedia
+from rr.gstreamer.media_manager import MediaManager
+
+#Use 8 or less, otherwise the display manager will throw an exception
+NUM_STREAMS = 8
+STREAM_NAME_PREFIX = "stream"
+TIME_TO_WAIT = 5
+
+def main():
+    print ("Display Manager Example Application")
+
+    media_manager = MediaManager()
+    display_manager = DisplayManager()
+
+    for i in range(NUM_STREAMS):
+        src = IMedia()
+        src_name = STREAM_NAME_PREFIX + str(i)
+        src_desc = "videotestsrc is-live=true ! video/x-raw,width=320,height=240 ! queue ! interpipesink async=false name=" + src_name 
+        src.create_media(src_desc)
+        media_manager.add_media(src_name, src)
+        display_manager.add_stream(src_name)
+
+    display_manager.create_display()
+    media_manager.play_media()
+    display_manager.play_display()
+
+    time.sleep(TIME_TO_WAIT)
+
+    display_manager.stop_display()
+    display_manager.delete_display()
+    media_manager.stop_media()
+        
+if __name__ == "__main__":
+    main()
