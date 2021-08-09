62fc02eb2f4feecd8e5318f1ffd58894ce566795
Daniel Chaves <daniel.chaves@ridgerun.com>
2021-08-09 13:43:50 -0600

    stream_manager: Set the test pipeline to one buffer

--- tests/stream/test_stream_manager.py
+++ tests/stream/test_stream_manager.py
@@ -37,7 +37,7 @@ class TestStreamManager(unittest.TestCase):
         ai_manager.process_image = MagicMock(
             mock_image, model, disp_width, disp_height)
 
-        desc = "videotestsrc is-live=true ! video/x-raw,width=640,height=480,format=BGRx ! appsink name=appsink async=false emit-signals=true"
+        desc = "videotestsrc num-buffers=1 is-live=true ! video/x-raw,width=640,height=480,format=BGRx ! appsink name=appsink async=false emit-signals=true"
         key = "media1"
 
         media = GstMedia()
