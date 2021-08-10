56e5956087961fc006f87923666a4577e0d13a45
Michael Gruner <michael.gruner@ridgerun.com>
2021-08-10 13:23:25 -0600

    action_manager: Implement triggers

--- rr/actions/action_manager.py
+++ rr/actions/action_manager.py
@@ -4,6 +4,7 @@
 #           Marisol Zeledon <marisol.zeledon@ridgerun.com>
 
 from rr.actions.log_event import LogEvent
+from copy import deepcopy
 
 
 class FilterError(RuntimeError):
@@ -58,3 +59,53 @@ class Action:
             return LogEvent.make(desc)
         else:
             raise ActionError('Unkown action "%s"' % atype)
+
+
+class TriggerError(RuntimeError):
+    pass
+
+
+class Trigger:
+    def __init__(self, name, action, filters):
+        self._name = name
+        self._action = action
+        self._filters = filters
+
+    def execute(self, prediction, image, media):
+        for filter in self._filters:
+            filter.apply(prediction)
+
+        self._action.execute(media, image, prediction, self._filters)
+
+    @classmethod
+    def make(cls, desc, all_actions, all_filters):
+        try:
+            name = desc["name"]
+            req_action = desc["action"]
+            req_filters = desc["filters"]
+        except KeyError as e:
+            raise TriggerError("Malformed trigger description") from e
+
+        action = None
+        for candidate in all_actions:
+            if candidate.get_name() == req_action:
+                action = candidate
+                break
+
+        if not action:
+            raise TriggerError('Unknown action "%s"' % req_action)
+
+        filters = []
+        for req in req_filters:
+            match = None
+            for candidate in all_filters:
+                if req == candidate.get_name():
+                    match = candidate
+                    break
+
+            if match is not None:
+                filters.append(match)
+            else:
+                raise TriggerError('Unknown filter "%s"' % req)
+
+        return Trigger(name, action, filters)
--- tests/actions/test_action_manager.py
+++ tests/actions/test_action_manager.py
@@ -4,9 +4,11 @@
 #           Marisol Zeledon <marisol.zeledon@ridgerun.com>
 
 import unittest
+from unittest.mock import MagicMock
 
 from rr.actions.action_manager import Filter, FilterError
 from rr.actions.action_manager import Action, ActionError
+from rr.actions.action_manager import Trigger, TriggerError
 
 
 class TestFilter(unittest.TestCase):
@@ -72,3 +74,92 @@ class TestAction(unittest.TestCase):
             Action.make(unknown)
 
         self.assertEqual('Unkown action "unknown"', str(e.exception))
+
+
+class MockTriggerFilter1:
+    def __init__(self):
+        self.apply = MagicMock()
+
+    def get_name(self):
+        return "mock_filter1"
+
+
+class MockTriggerFilter2:
+    def __init__(self):
+        self.apply = MagicMock()
+
+    def get_name(self):
+        return "mock_filter2"
+
+
+class MockTriggerAction:
+    def __init__(self):
+        self.execute = MagicMock()
+
+    def get_name(self):
+        return "mock_action"
+
+
+class MockTriggerImage:
+    pass
+
+
+class MockTriggerMedia:
+    pass
+
+
+class TestTrigger(unittest.TestCase):
+
+    def setUp(self):
+        self.desc = {
+            "name": "trigger_name",
+            "action": "mock_action",
+            "filters": [
+                "mock_filter1",
+                "mock_filter2",
+            ]
+        }
+
+        self.action = MockTriggerAction()
+        self.filter1 = MockTriggerFilter1()
+        self.filter2 = MockTriggerFilter2()
+        self.filters = [self.filter1, self.filter2]
+
+    def test_trigger_success(self):
+        trigger = Trigger.make(self.desc, [self.action], self.filters)
+        self.assertEqual('trigger_name', trigger._name)
+
+        image = MockTriggerImage()
+        media = MockTriggerMedia()
+        pred = {"mock": "prediction"}
+
+        trigger.execute(pred, image, media)
+        self.filter1.apply.assert_called_with(pred)
+        self.filter2.apply.assert_called_with(pred)
+        self.action.execute.assert_called_with(
+            media, image, pred, self.filters)
+
+    def test_trigger_malformed_desc(self):
+        # remove name
+        self.desc.pop("name")
+
+        with self.assertRaises(TriggerError) as e:
+            Trigger.make(self.desc, [self.action], self.filters)
+
+        self.assertEqual("Malformed trigger description", str(e.exception))
+
+    def test_trigger_unkown_action(self):
+        self.desc["action"] = "other_action"
+
+        with self.assertRaises(TriggerError) as e:
+            Trigger.make(self.desc, [self.action], self.filters)
+
+        self.assertEqual('Unknown action "other_action"', str(e.exception))
+
+    def test_trigger_unkown_filter(self):
+        self.desc["filters"].append("mock_filter3")
+
+        with self.assertRaises(TriggerError) as e:
+            Trigger.make(self.desc, [self.action], self.filters)
+
+        self.assertEqual('Unknown filter "mock_filter3"', str(e.exception))
