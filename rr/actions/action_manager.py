26c468a421d3d8821dff4a3207fbcfc23ef71608
Michael Gruner <michael.gruner@ridgerun.com>
2021-08-09 23:26:19 -0600

    action_manger: Implement filter

---
+++ rr/actions/action_manager.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+#  Copyright (C) 2021 RidgeRun, LLC (http://www.ridgerun.com)
+#  Authors: Daniel Chaves <daniel.chaves@ridgerun.com>
+#           Marisol Zeledon <marisol.zeledon@ridgerun.com>
+
+class FilterError(RuntimeError):
+    pass
+
+
+class Filter:
+    def __init__(self, name, labels, probability):
+        self._name = name
+        self._labels = labels
+        self._probability = probability
+        self._is_triggered = False
+
+    def apply(self, prediction):
+        self._is_triggered = False
+
+        for instance in prediction["instances"]:
+            for label in instance["labels"]:
+                if label["class"] in self._labels and label["probability"] >= self._probability:
+                    self._is_triggered = True
+                    return
+
+    def is_triggered(self):
+        return self._is_triggered
+
+    @classmethod
+    def make(cls, desc):
+        try:
+            name = desc["name"]
+            labels = desc["labels"] if isinstance(
+                desc["labels"], list) else [desc["labels"]]
+            probability = desc["probability"]
+        except KeyError as e:
+            raise FilterError("Malformed filter description") from e
+
+        return Filter(name, labels, probability)
---
+++ tests/actions/test_action_manager.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+#  Copyright (C) 2021 RidgeRun, LLC (http://www.ridgerun.com)
+#  Authors: Daniel Chaves <daniel.chaves@ridgerun.com>
+#           Marisol Zeledon <marisol.zeledon@ridgerun.com>
+
+import unittest
+
+from rr.actions.action_manager import Filter, FilterError
+
+
+class TestFilter(unittest.TestCase):
+    def setUp(self):
+        self._desc = {
+            "name": "test",
+            "labels": ["cat", "dog"],
+            "probability": 0.5
+        }
+
+    def test_make_invalid(self):
+        malformed = {"name": "value"}
+
+        with self.assertRaises(FilterError) as e:
+            Filter.make(malformed)
+
+        self.assertEqual("Malformed filter description", str(e.exception))
+
+    def common(self, desc, pred, expected):
+        filter = Filter.make(desc)
+        self.assertEqual(True, filter is not None)
+
+        filter.apply(pred)
+
+        self.assertEqual(expected, filter.is_triggered())
+
+    def test_is_triggered_single_label(self):
+        desc = {
+            "name": "test",
+            "labels": "cat",
+            "probability": 0.5
+        }
+
+        pred = {"instances": [
+            {"labels": [{"class": "cat", "probability": 0.5}]}]}
+        self.common(desc, pred, True)
+
+    def test_is_triggered_multi_label(self):
+        pred = {"instances": [
+            {"labels": [{"class": "dog", "probability": 0.5}]}]}
+
+        self.common(self._desc, pred, True)
+
+    def test_not_triggered_lower_prob(self):
+        pred = {"instances": [
+            {"labels": [{"class": "dog", "probability": 0.49}]}]}
+
+        self.common(self._desc, pred, False)
+
+    def test_not_triggered_no_class(self):
+        pred = {"instances": [
+            {"labels": [{"class": "snake", "probability": 0.9}]}]}
+
+        self.common(self._desc, pred, False)
