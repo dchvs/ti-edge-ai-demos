43743ad3a2c579e40e7ea8aa7c082552609a77be
Daniel Chaves <daniel.chaves@ridgerun.com>
2021-08-11 15:12:27 -0600

    stream_manager: Update the StreamManager test to support callback flow

--- tests/stream/test_stream_manager.py
+++ tests/stream/test_stream_manager.py
@@ -8,14 +8,20 @@ import time
 import unittest
 from unittest.mock import MagicMock
 
+from rr.actions.action_manager import ActionManager
+from rr.actions.action_manager import Trigger, TriggerError
 from rr.ai.ai_manager import AIManagerOnNewImage
 from rr.gstreamer.gst_media import GstMedia
 from rr.gstreamer.media_manager import MediaManager
-from rr.stream.stream_manager import StreamManager
 from rr.stream.stream_manager import OnNewImage
+from rr.stream.stream_manager import StreamManager
 
 
 class MockImage:
+    pass
+
+
+class MockTriggerMedia:
     pass
 
 
@@ -23,10 +29,57 @@ model = "/opt/edge_ai_apps/models/detection/TFL-OD-200-ssd-mobV1-coco-mlperf-300
 disp_width = 2040
 disp_height = 1920
 mock_image = MockImage()
+mock_trigger_media = MockTriggerMedia()
+
+
+class MockTriggerFilter1:
+    def __init__(self):
+        self.apply = MagicMock()
+
+    def get_name(self):
+        return "mock_filter1"
+
+
+class MockTriggerFilter2:
+    def __init__(self):
+        self.apply = MagicMock()
+
+    def get_name(self):
+        return "mock_filter2"
+
+
+class MockTriggerAction:
+    def __init__(self):
+        self.execute = MagicMock()
+
+    def get_name(self):
+        return "mock_action"
 
 
 class TestStreamManager(unittest.TestCase):
     def testsuccess(self):
+
+        self.desc = {
+            "name": "trigger_name",
+            "action": "mock_action",
+            "filters": [
+                "mock_filter1",
+                "mock_filter2",
+            ]
+        }
+
+        self.action = MockTriggerAction()
+        self.filter1 = MockTriggerFilter1()
+        self.filter2 = MockTriggerFilter2()
+        self.filters = [self.filter1, self.filter2]
+
+        trigger = Trigger(self.desc, self.action, self.filters)
+        action_manager = ActionManager(trigger)
+
+        prediction = {"mock": "prediction"}
+        action_manager.execute = MagicMock(
+            prediction, mock_image, mock_trigger_media)
+
         self.mock_on_new_prediction_cb = MagicMock()
         self.mock_on_new_postprocess_cb = MagicMock()
 
@@ -47,6 +100,7 @@ class TestStreamManager(unittest.TestCase):
         media_manager.add_media(key, media)
 
         stream_manager = StreamManager(
+            action_manager,
             ai_manager,
             media_manager,
             model,
@@ -56,6 +110,7 @@ class TestStreamManager(unittest.TestCase):
         stream_manager.play()
         time.sleep(1)
         ai_manager.process_image.assert_called_once()
+        action_manager.execute.assert_called_once()
 
 
 if __name__ == '__main__':
