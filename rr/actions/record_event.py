0d8131867eead83da178cdaada20b0aa569cabc9
Marisol Zeledon <marisol.zeledon@ridgerun.com>
2021-08-11 10:51:36 -0600

    recordevent: Add struct to hold media properties

--- rr/actions/record_event.py
+++ rr/actions/record_event.py
@@ -8,6 +8,13 @@ from threading import Timer, Lock
 from rr.actions.gst_recording_media import GstRecordingMedia
 
 
+class RecordingMedia():
+    def __init__(self, rec_media, recording, timer):
+        self.rec_media = rec_media
+        self.is_recording = recording
+        self.timer = timer
+
+
 class RecordEvent():
     def __init__(self, rec_dir):
         self._rec_dir = rec_dir
@@ -21,46 +28,41 @@ class RecordEvent():
         self._execute_mutex.acquire()
 
         self._check_dict(media_name)
+        media = self._medias_dict[media_name]
 
         if inf_filter.is_triggered():
-            rec_status = self._medias_dict[media_name]['recording']
 
-            if not rec_status:
+            if not media.is_recording:
                 timestamp = image.get_timestamp()
                 filename = self._rec_dir + "/" + "detection_recording_" + \
                     media_name + "_" + timestamp + ".ts"
-                self._medias_dict[media_name]['rec_media'] = GstRecordingMedia(
+                self._medias_dict[media_name].rec_media = GstRecordingMedia(
                     filename)
 
             self._start_timer(media_name, rec_time, inf_filter)
 
-        rec_status = self._medias_dict[media_name]['recording']
-        if rec_status:
-            rec_media = self._medias_dict[media_name]['rec_media']
-            rec_media.push_image(image)
+        if media.is_recording:
+            media.rec_media.push_image(image)
 
         self._execute_mutex.release()
 
     def _start_timer(self, media_name, rec_time, inf_filter):
         media = self._medias_dict[media_name]
 
-        if media['recording']:
-            media['timer'].cancel()
+        if media.is_recording:
+            media.timer.cancel()
 
-        media['timer'] = Timer(rec_time, self._stop_recording, [media_name])
-        media['recording'] = True
+        media.timer = Timer(rec_time, self._stop_recording, [media_name])
+        media.is_recording = True
 
-        media['timer'].start()
+        media.timer.start()
 
     def _check_dict(self, media_name):
         if media_name not in self._medias_dict:
-            self._medias_dict[media_name] = {}
-            self._medias_dict[media_name]['rec_media'] = None
-            self._medias_dict[media_name]['recording'] = False
-            self._medias_dict[media_name]['timer'] = None
+            self._medias_dict[media_name] = RecordingMedia(None, False, None)
 
     def _stop_recording(self, media_name):
         self._stop_rec_mutex.acquire()
-        self._medias_dict[media_name]['rec_media'].stop_media()
-        self._medias_dict[media_name]['recording'] = False
+        self._medias_dict[media_name].rec_media.stop_media()
+        self._medias_dict[media_name].is_recording = False
         self._stop_rec_mutex.release()
