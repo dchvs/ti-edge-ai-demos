3779ea078c9cdd77b9e32aa85283d4cfff02b4bf
Michael Gruner <michael.gruner@ridgerun.com>
2021-08-16 22:13:12 -0600

    log_event: Short circuit processing if filter was not triggered

--- rr/actions/log_event.py
+++ rr/actions/log_event.py
@@ -133,7 +133,14 @@ class LogEvent():
                                    'bbox-height': instance['bbox']['height']})
         self._file.flush()
 
-    def execute(self, media, image, inf_results):
+    def _is_triggered(self, filters):
+        for f in filters:
+            if f.is_triggered():
+                return True
+
+        return False
+
+    def execute(self, media, image, inf_results, inf_filter):
         """ Executes the log action
 
         Parameters
@@ -145,6 +152,9 @@ class LogEvent():
         inf_results : dict
             The inference results
         """
+
+        if not self._is_triggered(inf_filter):
+            return
 
         self._mutex.acquire()
         self._log(media, image, inf_results)
