800667c997ce1831d7ad14530f3c6916cd313652
Daniel Chaves <daniel.chaves@ridgerun.com>
2021-08-25 00:37:03 -0600

    display_manager: Remove stale test about adding stream with string desc instead of object

--- tests/display/test_display_manager.py
+++ tests/display/test_display_manager.py
@@ -86,7 +86,7 @@ class TestDisplayManager(unittest.TestCase):
         self.assertTrue(self.stream not in list)
 
     def test_create_display(self):
-        display_desc = "videomixer name=mixer  sink_0::xpos=0 sink_0::ypos=0 ! queue ! video/x-raw,width=1280,height=720 ! kmssink force-modesetting=true sync=false async=false  videotestsrc ! queue ! videoscale ! video/x-raw,width=320,height=240 ! mixer. "
+        display_desc = 'videomixer name=mixer  sink_0::xpos=0 sink_0::ypos=0 ! queue ! video/x-raw,width=1280,height=720 ! kmssink force-modesetting=true sync=false async=false  videotestsrc ! queue ! videoscale ! video/x-raw,width=320,height=240 ! mixer.'
         self.display_manager.create_display()
         self.assertEqual(
             display_desc,
@@ -151,10 +151,6 @@ class TestDisplayManagerFail(unittest.TestCase):
         with self.assertRaisesRegex(DisplayManagerError, "Invalid media object"):
             self.display_manager.add_stream(None)
 
-    def test_add_stream_not_string(self):
-        with self.assertRaisesRegex(DisplayManagerError, "Invalid media object"):
-            self.display_manager.add_stream(1)
-
     def test_add_stream_duplicated(self):
         with self.assertRaisesRegex(DisplayManagerError, "Stream already exists in display manager"):
             self.display_manager.add_stream(self.stream)
@@ -193,7 +189,7 @@ class TestDisplayManagerFail(unittest.TestCase):
             self.display_manager.remove_stream(self.stream_desc['id'])
 
     def test_create_display_empty(self):
-        self.display_manager.remove_stream(self.stream)
+        self.display_manager.remove_stream(self.stream_desc['id'])
         with self.assertRaisesRegex(DisplayManagerError, "No streams added"):
             self.display_manager.create_display()
 
@@ -203,14 +199,17 @@ class TestDisplayManagerFail(unittest.TestCase):
             self.display_manager.create_display()
 
     def test_play_display_not_created(self):
-        with self.assertRaisesRegex(DisplayManagerError, "Display description not created yet"):
+        self.display_manager.remove_stream(self.stream_desc['id'])
+        with self.assertRaisesRegex(DisplayManagerError, "Unable to start display"):
             self.display_manager.play_display()
 
     def test_stop_display_not_created(self):
-        with self.assertRaisesRegex(DisplayManagerError, "Display description not created yet"):
+        self.display_manager.remove_stream(self.stream_desc['id'])
+        with self.assertRaisesRegex(DisplayManagerError, "Unable to stop display"):
             self.display_manager.stop_display()
 
     def test_delete_display_not_created(self):
+        self.display_manager.remove_stream(self.stream_desc['id'])
         with self.assertRaisesRegex(DisplayManagerError, "Display description not created yet"):
             self.display_manager.stop_display()
 
